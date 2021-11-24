a3761d6510b5c8940141f5c400432900f1f275f9
salvikajal <94483005+salvikajal@users.noreply.github.com>
2021-11-24 14:31:25 -0500

    Update rock.c

--- 3_Implementation/src/rock.c
+++ 3_Implementation/src/rock.c
@@ -1,1 +1,15 @@
+int main() {
+int userChoice,compChoice;
+srand(time(0));
+printf("\t\t\t\t==========Welcome to Rock, Paper & Scissor Game==========");
+while(userChoice != 4){
+printf("\nPlease select the option: ");
+printf("\n1.Scissor");
+printf("\n2.Paper");
+printf("\n3.Rock");
+printf("\n4.Quit\n");
+scanf("%d",&userChoice);
 
+compChoice = (rand() % 3) + 1;
+checkOptions(userChoice,compChoice);
+}
